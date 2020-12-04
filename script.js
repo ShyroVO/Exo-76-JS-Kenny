b1623ab07b9da8f04677684080b84ad7f121bfee
ShyroVO <bosquet.laurie@outlook.com>
2020-12-04 16:26:33 +0100

    deplacment kenny fait

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-
-
+// Kenny monte
 document.getElementById('up').addEventListener('click',function() {
     let t = parseInt(document.getElementById('kenny').style.top);
     t = t - 10;
     document.getElementById('kenny').style.top = t + 'px';
 });
+
+// Kenny descend
+document.getElementById('down').addEventListener('click',function() {
+    let t = parseInt(document.getElementById('kenny').style.top);
+    t = t + 10;
+    document.getElementById('kenny').style.top = t + 'px';
+});
+
+// Kenny vas a gauche
+document.getElementById('left').addEventListener('click',function() {
+    let t = parseInt(document.getElementById('kenny').style.left);
+    t = t - 10;
+    document.getElementById('kenny').style.left = t + 'px';
+});
+
+// Kenny vas a droite
+document.getElementById('right').addEventListener('click',function() {
+    let t = parseInt(document.getElementById('kenny').style.left);
+    t = t + 10;
+    document.getElementById('kenny').style.left = t + 'px';
+});
+
