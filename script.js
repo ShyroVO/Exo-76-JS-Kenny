b7f25f320df01951f2ed19357cc86fc41bb7f0c0
ShyroVO <Bosquet.laurie@outlook.com>
2020-12-16 18:26:03 +0100

    zone rouge

--- script.js
+++ script.js
@@ -1,28 +1,62 @@
 // Kenny monte
 document.getElementById('up').addEventListener('click',function() {
     let t = parseInt(document.getElementById('kenny').style.top);
-    t = t - 10;
-    document.getElementById('kenny').style.top = t + 'px';
+    zoneRouge();
+
+    if (document.getElementById('kenny').style.top === "0px") {
+    }
+    else {
+        t = t - 10;
+        document.getElementById('kenny').style.top = t + 'px';
+    }
 });
 
 // Kenny descend
 document.getElementById('down').addEventListener('click',function() {
     let t = parseInt(document.getElementById('kenny').style.top);
-    t = t + 10;
-    document.getElementById('kenny').style.top = t + 'px';
+    zoneRouge();
+
+    if (document.getElementById('kenny').style.top === "470px") {
+    }
+    else {
+        t = t + 10;
+        document.getElementById('kenny').style.top = t + 'px';
+    }
 });
 
 // Kenny vas a gauche
 document.getElementById('left').addEventListener('click',function() {
     let t = parseInt(document.getElementById('kenny').style.left);
-    t = t - 10;
-    document.getElementById('kenny').style.left = t + 'px';
+    zoneRouge();
+
+    if (document.getElementById('kenny').style.left === "10px") {
+    }
+    else {
+        t = t - 10;
+        document.getElementById('kenny').style.left = t + 'px';
+    }
 });
 
 // Kenny vas a droite
 document.getElementById('right').addEventListener('click',function() {
     let t = parseInt(document.getElementById('kenny').style.left);
-    t = t + 10;
-    document.getElementById('kenny').style.left = t + 'px';
+    zoneRouge();
+
+    if (document.getElementById('kenny').style.left === "470px") {
+    }
+    else {
+        t = t + 10;
+        document.getElementById('kenny').style.left = t + 'px';
+    }
 });
 
+//Zone rouge:
+function zoneRouge (){
+
+    if (document.getElementById('kenny').style.top === "20px" && document.getElementById('kenny').style.left === "50px") {
+        document.getElementById('kenny').style.left === "200px";
+        document.getElementById('kenny').style.top === "200px";
+
+        alert("Kenny est mort");
+    }
+}
